2acaf6d8900b0cc84a4e0cccba97a496bddcde95
Arthure-code <2393288@cegeplimoilou.ca>
2025-11-30 16:37:12 -0500

    Ajout du cors.php dans config, ajout des configurations swagger dans le Controller.php

--- TP1_API_A25/app/Http/Controllers/Controller.php
+++ TP1_API_A25/app/Http/Controllers/Controller.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 /**
  * @OA\Info(title="Films API", version="1.0")
+ * @OA\Server(
+ *     url="http://localhost:8000",
+ *     description="API Server"
+ * )
  */
 abstract class Controller
 {
